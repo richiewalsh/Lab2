b594786df6e948dfb9c3a203e2cf1204bc8d0ee1
richiewalsh <richeywalsh37@gmail.com>
2024-05-04 10:39:14 +0000

    Failed first attempt error included
    Signed-off-by: richiewalsh <richeywalsh37@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include "mbed.h"
 #include <cstdio>
 
+//Error : Called object type 'int' is not a function or function pointer clang(typecheck_call_not_function)[Ln24, Col4]
+
+int i = 0;
+int j = 1;
+int k = 0;
+
+int fib(int n){
+
+if(i == n){
+
+    return i;
+
+}
+else{
+
+int fib = j + k;
+printf(fib);
+j = k;
+k = fib;
+i = i + 1;
+fib(i);
+
+}
+
+}
+
 // main() runs in its own thread in the OS
 int main()
 {
